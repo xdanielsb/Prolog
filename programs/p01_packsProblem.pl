24286808b8e0bbe7d968da89bc8af6b8643e836b
Daniel Santos <dfsantosbu@unal.edu.co>
2019-04-25 12:59:03 -0500

    Add argument of num of elements per box.
    Implement restriction boxes.

--- programs/p01_packsProblem.pl
+++ programs/p01_packsProblem.pl
@@ -12,7 +12,7 @@ date('April', 2019).
 % facts
 element(libro, 1, pesado, 2, no, no).
 element(libro, 2, pesado, 2, no, no).
-element(lampara, 2, mediano, 8, yes, no).
+element(lampara, 1, mediano, 8, yes, no).
 element(platos, 1, mediano, 5, yes, no).
 element(abrigo, 1, ligero, 3, no, no).
 element(tv, 1, pesado, 10, yes, no).
@@ -23,11 +23,16 @@ element(camisa, 2, ligero, 1, no, yes).
 
 % generate list of types O(n)
 fragiles(L) :- findall((M,X),element(_,M,_,X,yes,_), L).
-pesados(L) :- findall((M,X),element(_,M,_,X,no,_), L).
+% It is possible combine medium elements and heavy
+% pesados no fragiles
+pesados(L) :- findall((M,X),element(_,M,pesado,X,no,_), L).
+medianos(L) :- findall((M,X),element(_,M,mediano,X,no,_), L).
+% light elements could be combine with everyone
+ligeros(L) :- findall((M,X),element(_,M,ligero,X,no,_), L).
 
 % decompress tuples into list O(max(Xi))
 decompress((0, _), []):-!.
-decompress((A,X), [X|Ans]) :- B is (A-1), decompress((B, X), Ans).
+decompress((A,X), [(X,1)|Ans]) :- B is (A-1), decompress((B, X), Ans).
 
 % append lists O(n+m)
 append(L,[], L):-!.
@@ -38,28 +43,58 @@ append([X1|T1], L,[X1|T2]) :- append(T1, L, T2).
 boxCreate([], []):-!.
 boxCreate( [(X,Y)|T], L) :- boxCreate(T, Ans1 ), decompress((X,Y), Ans2), append(Ans1, Ans2, L).
 
-% Compress Boxes
+% Compress Boxes not restricttion of amount
 compress([], []).
-compress([X], [X]):-!.
-compress([X,Y|T], Ans):-
+compress([(X,Y)], [(X,Y)]):-!.
+compress([(X,C1),(Y, C2)|T], Ans):-
   N1 is X+Y,
+  CT is C1+C2,
   (
     (
       N1 =< 10,
-      append([N1], T, Ans1),
+      append([(N1,CT)], T, Ans1),
       compress(Ans1, Ans)
     );
     (
-      N1 > 10,((X =< Y, W is X, Z is Y);(Y < X, W is Y, Z is X) ),
-      append([W], T, Ans1),
+      (N1 > 10),
+      ((X =< Y, W is X, Z is Y, C3 is C1, C4 is C2);(Y < X, W is Y, Z is X, C3 is C2, C4 is C1) ),
+      append([(W, C3)], T, Ans1),
       compress(Ans1, Ans2),
-      append([Z], Ans2, Ans)
+      append([(Z, C4)], Ans2, Ans)
     )
   ).
 
-%  Unite the algorithm, omg the tricky part
-init(R1, R2) :-
-  fragiles(L1), pesados(L2), boxCreate(L1, B1), boxCreate(L2, B2), compress(B1, R1), compress(B2, R2).
+
+% Compress Boxes each box < 4 heavier elements
+compress2([], []).
+compress2([(X,Y)], [(X,Y)]):-!.
+compress2([(X,C1),(Y, C2)|T], Ans):-
+  N1 is X+Y,
+  CT is C1+C2,
+  (
+    (
+      N1 =< 10,
+      CT < 4,
+      append([(N1,CT)], T, Ans1),
+      compress2(Ans1, Ans)
+    );
+    (
+      (N1 > 10; CT>=4),
+      ((X =< Y, W is X, Z is Y, C3 is C1, C4 is C2);(Y < X, W is Y, Z is X, C3 is C2, C4 is C1) ),
+      append([(W, C3)], T, Ans1),
+      compress2(Ans1, Ans2),
+      append([(Z, C4)], Ans2, Ans)
+    )
+  ).
+
+
+% Create boxes of fragiles y pesados no ligeros
+createLightHeavy(R1, R2) :-
+  fragiles(L1), pesados(L2),
+   boxCreate(L1, B1), boxCreate(L2, B2),
+   compress(B1, R1), compress2(B2, R2).
+
+% try to join as many boxes of medium packages with heavy boxes
 
 
 % Insert data
